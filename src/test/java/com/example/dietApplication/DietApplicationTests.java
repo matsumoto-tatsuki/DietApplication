5ab621495679551557899f9a15d3768398f49937
matsumoto <matsumoto.tatsuki.2023.kensyu@gmail.com>
2023-06-16 09:06:06 +0900

    DevelopmentStart

---
+++ src/test/java/com/example/dietApplication/DietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dietApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
