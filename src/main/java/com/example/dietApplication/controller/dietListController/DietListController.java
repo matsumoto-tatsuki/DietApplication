e03f65b5bc7cbb4c02b9d41fb2a9030ce21f25dc
zaha <zaharyuuichi.kensyu@gmail.com>
2023-06-19 23:23:01 +0900

    diet_select 5%

---
+++ src/main/java/com/example/dietApplication/controller/dietListController/DietListController.java
@@ -0,0 +1,12 @@
+package com.example.dietApplication.controller.dietListController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DietListController {
+    @GetMapping("/dietList")
+    public String getDietList(){
+        return "diet_list";
+    }
+}
